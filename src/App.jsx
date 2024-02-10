4a9ad4c515e507d3b7876ec26757d61f7bc5e79e
Yash Chamoli <yashchamoli1234@gmail.com>
2024-02-10 20:58:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+
+import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+
+    </>
+  )
+}
+
+export default App
